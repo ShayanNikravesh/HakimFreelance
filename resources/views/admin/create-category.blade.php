62592ac4b9267ab5d449b59187181997140637ea
ShayanNikravesh <149324389+ShayanNikravesh@users.noreply.github.com>
2024-05-08 16:07:18 +0330

    Update create-category.blade.php

--- resources/views/admin/create-category.blade.php
+++ resources/views/admin/create-category.blade.php
@@ -153,6 +153,7 @@
                                     <div class="form-group row">
                                         <label class="col-3">دسته والد</label>
                                         <div class="col-9">
+											{{-- hi --}}
                                             <select class="form-control form-control-solid" type="text" name="parent_id">
                                                 <option value="">انتخاب کنید.</option>
                                                 @foreach ($parentCategories as $category)
