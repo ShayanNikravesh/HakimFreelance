1dccf99ce95470b1e86a71e2039c6125d4e3b244
ShayanNikravesh <149324389+ShayanNikravesh@users.noreply.github.com>
2024-07-14 00:35:26 +0330

    message page

--- resources/views/users/layouts/master.blade.php
+++ resources/views/users/layouts/master.blade.php
@@ -513,6 +513,18 @@
                                 </a>
                             </li>
                             <li>
+                                <a class="dropdown-item  py-2 fw-bold" href="{{route('message.index')}}">
+                                    <span class="pe-1">
+                                        <!--begin::Svg Icon-->
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-text-fill" viewBox="0 0 16 16">
+                                            <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1z"/>
+                                        </svg>
+                                        <!--end::Svg Icon-->
+                                    </span>
+                                    پیغام ها
+                                </a>
+                            </li>
+                            <li>
                                 <a class="dropdown-item  py-3 fw-bold" href="{{route('Exit')}}">
                                     <span class="pe-1">
                                         <!--begin::Svg Icon-->
@@ -908,6 +920,18 @@
                                         </svg><!--end::Svg Icon-->
                                     </span>
                                     نظرات
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item  py-2 fw-bold" href="{{route('message.index')}}">
+                                    <span class="pe-1">
+                                        <!--begin::Svg Icon-->
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-text-fill" viewBox="0 0 16 16">
+                                            <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1z"/>
+                                        </svg>
+                                        <!--end::Svg Icon-->
+                                    </span>
+                                    پیغام ها
                                 </a>
                             </li>
                             <li>
