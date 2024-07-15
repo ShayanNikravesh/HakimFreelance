b07d1ae07d4077387a034080bc26283344d97a5d
ShayanNikravesh <149324389+ShayanNikravesh@users.noreply.github.com>
2024-07-15 22:36:04 +0330

    Update master.blade.php

--- resources/views/users/layouts/master.blade.php
+++ resources/views/users/layouts/master.blade.php
@@ -1193,7 +1193,6 @@
             <!--Footer Top Detail:start-->
             <div class="footer-top-detial d-flex justify-content-between align-items-center">
                 <div class="">
-                    <a href="" title=""><img src="{{asset('userassets/img/logo-md.png')}}" alt=""></a>
                     <p class="mt-2 fs-7">تلفن پشتیبانی : 0000000</p>
                 </div>
                 <a href="#top" class="back-to-top border-radius-xl px-3 py-2 gray-600 fs-7">
